ef01d32d807b99a7d12cf8c3d57abc00982c7da8
Argana Phangquestian <arganaphangquestian@gmail.com>
2025-02-09 23:44:42 +0700

    Initial Commit

---
+++ Alkitab/AlkitabApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlkitabApp.swift
+//  Alkitab
+//
+//  Created by Argana Phangquestian on 09/02/25.
+//
+
+import SwiftUI
+
+@main
+struct AlkitabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
